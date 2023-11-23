00acea0ccc0bb8f95ac0abab6a994799451c6401
peroses <jonsjebe@gmail.com>
2023-11-23 09:08:56 +0700

    ubah dikit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,47 @@
-// Cart
+const ITEMS = [
+    {
+        id: 1,
+        name: "Keripik Tempe",
+        price: 10000,
+        image: '',
+        qty: 1
+    },
+    {
+        id: 2,
+        name: "Keripik Tempe",
+        price: 10000,
+        image: '',
+        qty: 1
+    },
+    {
+        id: 3,
+        name: "Keripik Tempe",
+        price: 10000,
+        image: '',
+        qty: 1
+    },
+    {
+        id: 4,
+        name: "Keripik Tempe",
+        price: 10000,
+        image: '',
+        qty: 1
+    },
+    {
+        id: 5,
+        name: "Keripik Tempe",
+        price: 10000,
+        image: '',
+        qty: 1
+    },
+]
 
+// Cart
 const openBtn = document.getElementById('hop');
 const cart = document.getElementById('sidecart');
 const closeBtn = document.getElementById('close_btn');
 const backdrop = document.querySelector('.backdrop');
+const itemsEl = document.querySelector('.katalog');
 
 openBtn.addEventListener('click', openCart);
 closeBtn.addEventListener('click', closeCart);
@@ -26,6 +64,21 @@ function closeCart() {
         backdrop.style.display = 'none'
     }, 500)
 }
+
+// Render Items
+function renderItems(){
+    ITEMS.forEach((item) => {
+        const itemEl = document.createElement('div')
+        itemEl.classList.add('card')
+        itemEl.innerHTML = `
+        <img src="${item.image}" alt="Denim Jeans">
+        <h1>Keripik Tempe</h1>
+        <p class="price">$19.99</p>
+        <p><button>Add to Cart</button></p>
+        `
+    })
+}
+
 // toggle class active
 
 const navbarNav = document.querySelector('.nav-list');
